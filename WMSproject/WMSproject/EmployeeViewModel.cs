b3cc7f270ee54c6b47763f561d28d335552aeb84
SportyCeca <nagy.marcell.oliver@gmail.com>
2026-02-03 10:40:45 +0100

    feladat

--- WMSproject/WMSproject/EmployeeViewModel.cs
+++ WMSproject/WMSproject/EmployeeViewModel.cs
@@ -3,5 +3,9 @@
     class EmployeeViewModel
     {
         public string Name { get;  } = "Nagy Anna";
+        public string TaxNumber { get; } = "32469677-2-17";
+
+        public int DaysOff { get; } = 367718400;
+
     }
 }
